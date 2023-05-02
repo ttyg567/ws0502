4a144305edf0f994b05a29ebe4e318241781d788
ttyg567@gmail.com <ttyg567@gmail.com>
2023-05-02 17:41:11 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/controller/ItemController.java
@@ -0,0 +1,16 @@
+package com.kbstar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/tables")
+public class ItemController {
+
+
+}
